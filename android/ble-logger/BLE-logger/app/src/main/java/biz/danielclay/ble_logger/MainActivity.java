7b08e12adf3cf051287ab1706013e74d5e1759a8
Daniel Clay <dclay.daniel@gmail.com>
2017-06-01 17:58:06 +0100

    Add BLE support and check that Bluetooth is enabled on startup

--- android/ble-logger/BLE-logger/app/src/main/java/biz/danielclay/ble_logger/MainActivity.java
+++ android/ble-logger/BLE-logger/app/src/main/java/biz/danielclay/ble_logger/MainActivity.java
@@ -1,13 +1,31 @@
 package biz.danielclay.ble_logger;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BluetoothAdapter mBluetoothAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setup Bluetooth adapter
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Check that Bluetooth is enabled
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
     }
 }
