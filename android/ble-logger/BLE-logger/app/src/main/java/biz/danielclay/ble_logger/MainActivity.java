c71ea8c0d9a061e82bceca01494c8287dcde3fb9
Daniel Clay <dclay.daniel@gmail.com>
2017-06-01 17:28:35 +0100

    Init Android project and design UI

---
+++ android/ble-logger/BLE-logger/app/src/main/java/biz/danielclay/ble_logger/MainActivity.java
@@ -0,0 +1,13 @@
+package biz.danielclay.ble_logger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
